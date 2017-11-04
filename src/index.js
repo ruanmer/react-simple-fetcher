3dc1ad287b84b08a272dc8b3bafc6cfa073a818d
Ruan Mer <ruanrm@gmail.com>
2017-11-04 20:24:41 -0200

    Passing fetching prop

--- src/index.js
+++ src/index.js
@@ -1,19 +1,28 @@
 import React, { PureComponent } from 'react';
 
 export default class Fetcher extends PureComponent {
+  constructor (props) {
+    super(props);
+
+    this.state = {
+      fetching: true
+    }
+  }
+
   render () {
     const { component, render, children } = this.props;
+    const props = this.state;
 
     if (component) {
-      return React.createElement(component);
+      return React.createElement(component, props);
     }
 
     if (render) {
-      return render();
+      return render(props);
     }
 
     if (typeof children === 'function') {
-      return children();
+      return children(props);
     }
 
     if (children && React.Children.count(children) > 0) {
--- test/index.js
+++ test/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import Fetcher from '../src';
 
-const Component = () => <div>Component</div>;
-
 test('should render component prop', () => {
+  const Component = props => <div {...props}>Component</div>;
+
   const tree = renderer.create(
     <Fetcher component={Component} />
   ).toJSON();
@@ -14,8 +14,8 @@ test('should render component prop', () => {
 
 test('should render prop', () => {
   const tree = renderer.create(
-    <Fetcher render={()=> (
-      <div>Render</div>
+    <Fetcher render={props => (
+      <div {...props}>Render</div>
     )} />
   ).toJSON();
 
@@ -25,8 +25,8 @@ test('should render prop', () => {
 test('should render children when is function', () => {
   const tree = renderer.create(
     <Fetcher>
-      {() => (
-        <div>Children Function</div>
+      {props => (
+        <div {...props}>Children Function</div>
       )}
     </Fetcher>
   ).toJSON();
